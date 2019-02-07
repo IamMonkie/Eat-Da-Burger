3b410dc49e26fbe525974a030280b4a520e7df5f
IamMonkie <monkie_225@yahoo.com>
2019-02-07 17:42:56 -0500

    broke a lot of stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
-var routes = require("./controllers/burger_controller");
+var routes = require("./controllers/burger_controller.js");
 // =============================================================
 var app = express();
 
@@ -11,10 +11,10 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 // =============================================================
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
 // =============================================================
