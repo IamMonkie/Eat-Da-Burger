6fcb5b423eca442630af692fcd7c8db42412c297
IamMonkie <monkie_225@yahoo.com>
2019-04-05 12:49:46 -0400

    update sql server info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burger_db"
+// });
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "fnx6frzmhxw45qcb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
   port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
+  user: "gt0bymvg667zkqgm",
+  password: "ccpi5agpgcx5begm",
+  database: "szfbazl6118fed79"
 });
 
 connection.connect(function(err) {
